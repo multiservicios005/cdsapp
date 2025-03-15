c683cf378ba030e62c69cd2e0a72c71e637edabe
Luis <multiservicios005@gmail.com>
2025-03-15 18:07:33 -0300

    Versión 20250315 - Se agrega la funcionalidad para importar los datos a partir del archivo json de respaldo para restaurar la base de datos. También se depura el archivo application.properties.

---
+++ src/main/java/com/servicios5estrellas/importar/controller/RestImportController.java
@@ -0,0 +1,41 @@
+package com.servicios5estrellas.importar.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.servicios5estrellas.model.Cliente;
+import com.servicios5estrellas.model.Orden_De_Trabajo;
+import com.servicios5estrellas.model.ServicioOT;
+import com.servicios5estrellas.repository.IClienteRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class RestImportController {
+	
+	@Autowired
+	 private IClienteRepository repocli;
+
+	@PostMapping("/importDatos")
+	public void save(@RequestBody List<Cliente> clientes, HttpServletRequest req) {
+		System.out.println("RestImportController.save ");
+		System.out.println("clientes size  "+clientes.size());
+		
+		for (Cliente cliente : clientes) {		
+			for (Orden_De_Trabajo ot : cliente.getOrdenes_de_trabajo()) {
+				ot.setCliente(cliente);
+				for (ServicioOT servicio : ot.getServicios()) {
+					servicio.setOt(ot);
+				}
+			}
+			repocli.save(cliente);
+		}
+		
+		System.out.println("Importación terminada ...  ");
+		
+	}
+}
